40513927362b75ec2870809c0e6a4b8f9fb84cc3
ingri_000 <ingri_000@Equipo_Paola>
2014-09-11 16:10:54 -0500

    segunda subida

---
+++ practicas/segundaprueba.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ingri_000
+ */
+public class segundaprueba {
+    public static void main (String []args){
+    System.out.print("esta prueba es para la segunda subida");           
+}
+}
