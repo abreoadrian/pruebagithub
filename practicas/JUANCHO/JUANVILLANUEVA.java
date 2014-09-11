3e350a6a7435ce10cd0319b14c6bd5d88c645b10
USUARIO <USUARIO@USER>
2014-09-11 17:25:17 -0500

    MOISES SILVA ES MEJOR

---
+++ practicas/JUANCHO/JUANVILLANUEVA.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JUANCHO;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class JUANVILLANUEVA {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int sumar=0;
+     int sum=0;   
+        for (int i=1; i<50; i=i+1){
+        sumar=2*i;  
+        sum=sum+sumar;
+        System.out.println(sumar);
+        
+        }
+        
+        System.out.println(" RESULTADO "+sum);
+    }
+    
+}
